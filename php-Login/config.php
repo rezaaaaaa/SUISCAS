66e73ae9812a335a0f113dfd0a7f17f5d4c20d2f
reza <rsabet1984@gmail.com>
2015-10-31 19:22:36 +0800

    here is login folder

---
+++ php-Login/config.php
@@ -0,0 +1,7 @@
+<?php
+	$host="localhost"; // Host name 
+	$username="root"; // Mysql username 
+	$password=""; // Mysql password 
+	$db_name="suiscas"; // Database name 
+	$tbl_name="members"; // Table name
+?>
